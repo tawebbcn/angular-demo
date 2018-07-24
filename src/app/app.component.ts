467060f8feae10ce2bf8c9562e8c97bf53c91817
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-07-24 16:11:13 +0200

    angular-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hello world';
+  showForm = false;
+  name = '';
+  country = '';
+  students = [{
+    name: 'Andrea',
+    country: 'Spain'
+  }, {
+    name: 'Gabriel',
+    country: 'Colombia'
+    }, {
+    name: 'Daniela',
+    country: 'Venezuela'
+  }];
+
+  handleAddClick() {
+    this.students.push({
+      name: this.name,
+      country: this.country
+    });
+    this.name = '';
+    this.country = '';
+    this.showForm = false;
+  }
+
+  handleRemoveClick(index) {
+    this.students.splice(index, 1);
+  }
 }
